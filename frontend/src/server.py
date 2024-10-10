82392304482179a9404bf0b171dd0a9ff06d6caa
Phil Andreoli <phil.andreoli@gmail.com>
2024-10-10 08:35:03 -0500

    The thread id from the streamlit app was blank.

--- frontend/src/server.py
+++ frontend/src/server.py
@@ -32,7 +32,7 @@ if "messages" not in st.session_state:
             """
         }
     ]
-    st.session_state["thread_id"] = uuid4()
+    st.session_state["thread_id"] = str(uuid4())
 
 for message in st.session_state["messages"]:
     st.chat_message(message["role"]).write(message["content"])
